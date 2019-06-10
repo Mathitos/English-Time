4bd49bde6bc34d99e7759d2dc2d72971f977a14b
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-06-10 01:23:50 -0300

    add button to play the sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,11 +9,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
+        body: EnglishButton(),
+      ),
+    );
+  }
+}
+
+void playSound() {
+  Fluttertoast.showToast(
+    msg: "should play a sound",
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM, // also possible "TOP" and "CENTER"
+    backgroundColor: new Color(0xffe74c3c),
+    textColor: new Color(0xffffffff),
+  );
+  // AudioPlayer audioPlayer = AudioPlayer();
+  // audioPlayer.play("assets/english-motherfucker-do-you-speak-it.mp3");
+}
+
+class EnglishButton extends StatelessWidget {
+  EnglishButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: playSound,
+        child:
+            const Text('English MotherFucker', style: TextStyle(fontSize: 20)),
       ),
     );
   }
